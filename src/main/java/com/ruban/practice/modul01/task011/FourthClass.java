9b627f9d271d8bf81f31ace4b8874ad231673af7
Ольга <mymail.ruban@gmail.com>
2017-02-01 00:50:32 +0200

    Initial commit

---
+++ src/main/java/com/ruban/practice/modul01/task011/FourthClass.java
@@ -0,0 +1,7 @@
+package com.ruban.practice.modul01.task011;
+
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("My first class on Java");
+    }
+}
